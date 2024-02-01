cace023e32ba447f13411967f6e5d3e03d00d0e0
Spencer Wilson <spencer.wilson@uwaterloo.ca>
2024-02-01 16:37:10 -0500

    fail more restrictively

--- scripts/release-test-ci.sh
+++ scripts/release-test-ci.sh
@@ -1,5 +1,3 @@
-#/bin/sh
+#/bin/bash
 
-git branch --show-current
-cd cicd-playground-upstream
-git branch --show-current
+git branch --show-current && cd cicd-playground-upstream && git branch --show-current
